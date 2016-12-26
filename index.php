3b38a87f3a94668e45af3b237316f44b1f928909
Kevinster73 <kevinster73@gmail.com>
2016-12-26 20:01:52 +0700

    salah ojan

--- index.php
+++ index.php
@@ -70,7 +70,7 @@ $app->post('/uploadFile', function () use ($Connection) {
 $app->get('/gallery', function () use ($Connection){
   $ObjectsListResponse = $Connection->list_objects('my-new-bucket');
   $Objects = $ObjectsListResponse->body->Contents;
-  $app->view->setTemplatesDirectory('./s3-frontend');
+  $app->view()->setTemplatesDirectory('./s3-frontend');
   $app->render('gallery.php', array('Objects' => $Objects ));
 });
 
