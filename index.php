fdb9b1e1d2a42b7e96d69ba38f5ffc8ce5004f51
prakashdivyy <prakashdivyy@gmail.com>
2016-12-27 03:51:41 +0700

    Fix tampilan busuk

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ define('AWS_KEY', 'RKAG1I1VD21A7ZD0DN84');
 define('AWS_SECRET_KEY', 'cEZgXggQOFH7ns8iPDRtFJffog8XjJOfTOt2wZnR');
 define('AWS_CANONICAL_ID', 'grup2');
 define('AWS_CANONICAL_NAME', 'grup2');
-
-$HOST = 'grup2-ceph-04.sisdis.ui.ac.id';
+define('HOST', 'grup2-ceph-04.sisdis.ui.ac.id');
+define('BUCKET_NAME', 'my-new-bucket');
 
 $Connection = new AmazonS3(array(
     'key' => AWS_KEY,
@@ -15,7 +15,7 @@ $Connection = new AmazonS3(array(
     'canonical_name' => AWS_CANONICAL_NAME,
 ));
 
-$Connection->set_hostname($HOST);
+$Connection->set_hostname(HOST);
 
 $Connection->allow_hostname_override(false);
 
@@ -23,7 +23,7 @@ $Connection->enable_path_style();
 
 $app = new \Slim\Slim();
 
-$app->get('/', function () use ($Connection) {
+$app->get('/list', function () use ($Connection) {
     $ListResponse = $Connection->list_buckets();
     $Buckets = $ListResponse->body->Buckets->Bucket;
     foreach ($Buckets as $Bucket) {
@@ -37,7 +37,7 @@ $app->get('/uploaded', function () use ($Connection) {
     $Objects = $ObjectsListResponse->body->Contents;
     foreach ($Objects as $Object) {
         echo $Object->Key . "\t" . $Object->Size . "\t" . $Object->LastModified . "\n";
-	      echo "<br/>";
+        echo "<br/>";
     }
 });
 
@@ -48,36 +48,38 @@ $app->get('/create/:name', function ($name) use ($Connection) {
 });
 
 
-
 $app->get('/download/:name', function ($name) use ($Connection) {
     $url = $Connection->get_object_url('my-new-bucket', $name . '.txt', '1 hour');
     $url = preg_replace("/^http:/i", "https:", $url);
     echo $url . "\n";
 });
 
-$app->post('/uploadFile', function () use ($Connection, $app) {
-  $filename = basename($_FILES["fileToUpload"]["name"]);
-  $size = $_FILES["fileToUpload"]["size"];
-  $file_resource =  fopen($_FILES["fileToUpload"]["tmp_name"], 'r');
-  $response = $Connection->create_object('my-new-bucket', $filename, array(
-    'fileUpload' => $file_resource,
-    'length' => $size,
-    'acl' => AmazonS3::ACL_PUBLIC
-  ));
-  if ($response->isOK()){
-    $app->view()->setTemplatesDirectory('./');
-    $app->render('s3-frontend/get.php', array('filename' => $filename ));
-  } else {
-    $app->redirect('s3-frontend/index.html');
-  }
-
+$app->get('/', function () use ($app) {
+    $app->render('home.php', array('error' => 0));
 });
 
-$app->get('/gallery', function () use ($Connection, $app){
-  $ObjectsListResponse = $Connection->list_objects('my-new-bucket');
-  $Objects = $ObjectsListResponse->body->Contents;
-  $app->view()->setTemplatesDirectory('./');
-  $app->render('s3-frontend/gallery.php', array('Objects' => $Objects ));
+$app->post('/', function () use ($Connection, $app) {
+    $filename = basename($_FILES["fileToUpload"]["name"]);
+    $size = $_FILES["fileToUpload"]["size"];
+    $file_resource = fopen($_FILES["fileToUpload"]["tmp_name"], 'r');
+    $response = $Connection->create_object('my-new-bucket', $filename, array(
+        'fileUpload' => $file_resource,
+        'length' => $size,
+        'acl' => AmazonS3::ACL_PUBLIC
+    ));
+    if ($response->isOK()) {
+        $uploadURL = 'https://' . HOST . '/' . BUCKET_NAME . '/' . $filename;
+        $app->render('success.php', array('url' => $uploadURL, 'filename' => $filename));
+    } else {
+        $app->render('home.php', array('error' => 1));
+    }
+});
+
+$app->get('/gallery', function () use ($Connection, $app) {
+    $ObjectsListResponse = $Connection->list_objects('my-new-bucket');
+    $Objects = $ObjectsListResponse->body->Contents;
+    $app->view()->setTemplatesDirectory('./');
+    $app->render('s3-frontend/gallery.php', array('Objects' => $Objects));
 });
 
 $app->run();
